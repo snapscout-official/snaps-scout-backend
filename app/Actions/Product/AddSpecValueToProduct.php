85a1a8bd85b11a97143b51c5ca135bd7c4a2f58d
g610g <gio.gonzales@carsu.edu.ph>
2023-10-26 23:03:49 +0800

    final output added catcher for post

--- app/Actions/Product/AddSpecValueToProduct.php
+++ app/Actions/Product/AddSpecValueToProduct.php
@@ -26,9 +26,18 @@ class AddSpecValueToProduct
             if (empty($spec)) {
                 throw new ProductException("error adding spec value on spec {$request->specName}");
             }
+            $existingProductValuesName = $spec->values()->wherePivot('product_id', $product->product_id)
+                ->get()
+                ->map(function ($value) {
+                    return $value->spec_value;
+                })->toArray();
+            // dd($existingProductValuesName);
             //store each id of the specValue into an array
             $arrId = [];
             foreach ($request->specValues as $value) {
+                if (in_array($value, $existingProductValuesName)) {
+                    continue;
+                }
                 $arrId[] = SpecValue::firstOrCreate([
                     'spec_value' => $value,
                 ])->id;
