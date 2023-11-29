ecdc5e48b894c2ebe9f640b4bcf6e7e60661dddb
g610g <gio.gonzales@carsu.edu.ph>
2023-11-29 12:02:04 +0800

    added mongodb

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Merchant;
+use App\Models\MerchantProduct;
 use App\Models\ParentCategory;
 use App\Models\Spec;
 use Illuminate\Database\Query\Builder;
@@ -11,6 +13,13 @@ class TestController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return asset('storage/SnapScout.xlsx');
+        $merchantTest = new MerchantProduct([
+            'product_name' => 'Shampoo',
+            'quantity' => 20,
+            'price' => 2000
+        ]);
+        $merchantTest->save();
+        
+        return $merchantTest;
     }
 }
