d281afc1f072214dd7ac0a89bc44bd629e4cc04b
Vesperon <sinitchikoduo2@gmail.com>
2023-12-08 10:40:30 +0800

    Setting up MongoDB

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -92,6 +92,15 @@ return [
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
+
+        'mongodb' => [
+            'driver' => 'mongodb',
+           'host' => env('DB_HOST', '127.0.0.1'),
+           'port' => env('DB_PORT', 27017),
+           'database' => env('DB_DATABASE', 'myappdb'),
+           'username' => env('DB_USERNAME', ''),
+           'password' => env('DB_PASSWORD', '')
+         ],
 
     ],
 
